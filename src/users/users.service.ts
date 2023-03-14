9e49e9ce1bd1d81ce5de6dbb07e459c8b32ba3bf
RaWaiAung <rawaiucsm@gmail.com>
2023-03-14 12:20:04 +0630

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+    private users: any = [{ id: '1', name: 'Ra Wai' }];
+
+    findAll(name?: string): User[] {
+        if (name) {
+            return this.users.filter(user => user.name === name);
+        }
+        return this.users;
+    }
+
+    findById(userId: number): User {
+        return this.users.find(user => user.id === userId);
+    }
+
+    createUser(createUserDto: CreateUserDTO): User {
+        const newUser = { id: Date.now(), name: createUserDto.name };
+        this.users.push(newUser);
+        return newUser;
+    }
+}
